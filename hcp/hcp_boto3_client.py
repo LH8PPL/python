d9518640492d3c576c856557a37154456343706f
LH8PPL <liorhollander@gmail.com>
2023-03-04 22:50:13 +0200

    Update hcp_boto3_client.py

--- hcp/hcp_boto3_client.py
+++ hcp/hcp_boto3_client.py
@@ -1,16 +1,25 @@
+
 import boto3
 import urllib3
 # import the builtin time module
 import time
 import pprint
+from configparser import ConfigParser
+import configparser
+import argparse
+import logging
+import botocore
+
+logger = logging.getLogger()
 
 
-urllib3.disable_warnings()
-
-
-def boto3connection(access_key, secret_key, bucket_name, url):
+def boto3connection(access_key, secret_key, url):
     host = url
-    s3 = boto3.client('s3', aws_access_key_id=access_key, aws_secret_access_key=secret_key, endpoint_url=host, verify=False)
+    try:
+        s3 = boto3.client('s3', aws_access_key_id=access_key, aws_secret_access_key=secret_key, endpoint_url=host, verify=False)
+    except Exception as error:
+        logger.error("Exception Occurred while code Execution: " + str(error))
+        s3 = False
     return s3
 
 
@@ -65,7 +74,7 @@ def clean_empty_blocks(client, bucket_name):
         # print(block)
         if not check_if_empty(block, client, bucket_name):
             print(f"[{i+1}/{blocks_count}] Deleting empty block {block['Key']}")
-            delete_block(bucket_name, block['Key'], client)
+            # delete_block(bucket_name, block['Key'], client)
         else:
             print(f"[{i+1}/{blocks_count}] Discovered block: {block['Key']}")
             non_empty_blocks.append(block)
@@ -76,20 +85,69 @@ def clean_empty_blocks(client, bucket_name):
         f"Discovered {non_empty_blocks_count} non-empty blocks.")
 
 
+def read_config(filename):
+    # Read config.ini file
+    # We initiate the ConfigParser and read the file with read
+    cfg = ConfigParser()
+    cfg.read(filename)
+    # Get Sections names
+    print("Sections : ", cfg.sections())
+    return cfg
+
+
+urllib3.disable_warnings()
+
+opts = argparse.Namespace(start=time.time())
+next_time_to_print = 0
+wait_time = 3600
+
 # variables
-x = 'access_key'
-y = 'secret_key'
-z = 'bucket_name'
-t = 'https://url'
+access_key = 'IwloBJGw7LORC1hF'
+secret_key = 'vaqXmfMVdr7bZLgJ54NWDbMkPdXCJFpK'
+bucketname = 'test1'
+url = 'http://127.0.0.1:9000'
+
+file_name = 'config.ini'
 
 if __name__ == "__main__":
     while True:
         # Grab Current Time Before Running the Code
         start = time.time()
-        # initialize connection
-        connection = boto3connection(x, y, z, t)
-        # run function to search and delete for empty directories
-        clean_empty_blocks(connection, z)
+        current_time_passed = time.time() - opts.start
+        t = time.localtime()
+        current_time = time.strftime("%H:%M:%S", t)
+        print("Current Time =", current_time)
+        # wait to read config file
+        if current_time_passed >= next_time_to_print:
+            next_time_to_print += wait_time
+            logging.debug("Reading ini file [{}]".format(file_name))
+            try:
+                config = read_config(file_name)
+                print(f'Read config file and {current_time_passed=}')
+            except configparser.ParsingError as e:
+                logging.debug(e)
+        if config:
+            for section in config.sections():
+                print(f"\n")
+                print("SECTION -> ", section)
+                print("-------------------")
+                # initialize the connection variables
+                access_key = config.get(section, 'ACCESS_KEY')
+                secret_key = config.get(section, 'SECRET_KEY')
+                bucketname = config.get(section, 'BUCKET_NAME')
+                url = config.get(section, 'URL')
+                # initialize connection
+                connection = boto3connection(access_key, secret_key, url)
+                # run function to search and delete for empty directories
+                if connection:
+                    try:
+                        clean_empty_blocks(connection, bucketname)
+                    except Exception as error:
+                        logger.error("Exception Occurred while code Execution: " + str(error))
+                else:
+                    print("connection is not defined")
+        else:
+            print("config is not defined")
         # Grab Current Time After Running the Code
         end = time.time()
         # Subtract Start Time from The End Time
